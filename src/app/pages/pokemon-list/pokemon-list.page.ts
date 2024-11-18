8d8cb225319f14d4941d8548e5d0ae902123219f
Alejo-Py <marcelo.pinzon@epn.edu.ec>
2024-11-18 17:05:20 -0500

    Agregar servicio para obtener la lista de Pokémon y actualizar la página de lista de Pokémon

--- src/app/pages/pokemon-list/pokemon-list.page.ts
+++ src/app/pages/pokemon-list/pokemon-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-list.page.scss'],
 })
 export class PokemonListPage implements OnInit {
-  
-  constructor() { }
+  private readonly API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=150'; // constante inmutable
+  public pokemonList: any[] = []; // tipado más específico
+  public isLoading = false; // indicador de carga
 
-  ngOnInit() {
+  constructor(private pokemonService : PokemonService) {}
+
+  ngOnInit(): void {
+    this.getPokemonList();
   }
 
+
+public getPokemonList(): void {
+  this.isLoading = true;
+  this.pokemonService.getPokemonList()
+    .subscribe({
+      next: (data) => {
+        this.pokemonList = data.results;
+        console.log(this.pokemonList);
+      },
+      error: (err) => {
+        console.error('Error fetching Pokémon list:', err);
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+}
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  private readonly API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=150';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getPokemonList(): Observable<{ results: any[] }> {
+    return this.http.get<{ results: any[] }>(this.API_URL);
+  }
 }
